8ce0c3371d1a3aade22bee9a947d5ede38b19d9c
Alexx <114208686+ALEXXHUMILDE@users.noreply.github.com>
2023-03-25 21:54:24 -0600

    Delete Database directory

--- Database/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectDatabase() {
-    await mongoose.connect("mongodb+srv://alexxd:Z0LoZDRLKVtZN9Uu@alexxd.rrxvmj8.mongodb.net/?retryWrites=true&w=majority")
-    console.log("[MongoDB] ¡Se ha conectado correctamente la Base de Datos.")
-}
-
-module.exports = connectDatabase
